2cc845d7c0bd4bd4923f50f03e7af96eb6d77d63
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-06 10:19:16 +0100

    feat: add IRepository Interface

--- Exercice01API/Repositories/IRepository.cs
+++ Exercice01API/Repositories/IRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Exercice01API.Repositories
+﻿using System.Linq.Expressions;
+
+namespace Exercice01API.Repositories
 {
-    public interface IRepository
+    public interface IRepository<TEntity>
     {
+        bool Add(TEntity task);
+        TEntity? GetById(int id);
+        TEntity? Get(Expression<Func<TEntity, bool>> predicate);
+        List<TEntity> GetAll();
+        List<TEntity> GetAll(Expression<Func<TEntity, bool>> predicate);
+        bool Update(TEntity task);
+        bool Delete(int id);
     }
 }
