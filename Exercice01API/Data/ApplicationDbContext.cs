539cc48e63f10e6c4d8d2a8dc068e896e7f41ab9
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-06 16:18:25 +0100

    feat: update dbcontext

--- Exercice01API/Data/ApplicationDbContext.cs
+++ Exercice01API/Data/ApplicationDbContext.cs
@@ -10,7 +10,7 @@ namespace Exercice01API.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
-
+#nullable disable
         public DbSet<Contact> Contacts { get; set; }
 
 
