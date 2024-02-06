2065c8962a869ff1f1a657d61ede7afb4dda3f08
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-06 15:31:42 +0100

    feat: update programcs

--- Exercice01API/Program.cs
+++ Exercice01API/Program.cs
@@ -1,4 +1,6 @@
 using Exercice01API.Data;
+using Exercice01API.Models;
+using Exercice01API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IRepository<Contact>, ContactRepository>();
 
 var app = builder.Build();
 
