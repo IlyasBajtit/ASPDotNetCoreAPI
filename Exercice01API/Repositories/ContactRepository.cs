503b4544007f49f0ee01623918ca7c7d475d9f88
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-06 10:24:39 +0100

    feat: add Irepository contact

--- Exercice01API/Repositories/ContactRepository.cs
+++ Exercice01API/Repositories/ContactRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace Exercice01API.Repositories
+﻿using Exercice01API.Models;
+using System.Linq.Expressions;
+
+namespace Exercice01API.Repositories
 {
-    public interface ContactRepository
+    public interface ContactRepository : IRepository<Contact>
     {
+        public bool Add(Contact entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact? Get(Expression<Func<Contact, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Contact> GetAll(Expression<Func<Contact, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Contact? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Contact entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
