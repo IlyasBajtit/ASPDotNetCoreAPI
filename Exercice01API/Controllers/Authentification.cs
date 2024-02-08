d1d02e9c05cb77495df5f456ccc3b125c4dc239b
IlyasBajtit <ilyas.bajtit@gmail.com>
2024-02-08 10:10:21 +0100

    feat: create Authentification controller

---
+++ Exercice01API/Controllers/Authentification.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exercice01API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Authentification : ControllerBase
+    {
+    }
+}
